821c2b06d938db300a2d6d5aec3c53d65421ece0
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-03-07 02:32:10 +0700

    update

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -8,4 +8,5 @@
 <script src="{{ asset('material/assets/js/app.js') }}"></script>
 <script src=https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js></script>
 <script src=https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js></script>
+@include('sweetalert::alert')
 @stack('js')
