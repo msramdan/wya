86a31bad471799b5442b894038e1c218551e1ab8
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-04-16 17:42:57 +0700

    feat : add dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,27 +3,293 @@
 @section('title', __('Dashboard'))
 
 @section('content')
-    <div class="page-heading">
-        <h3>Dashboard</h3>
-    </div>
-
     <div class="page-content">
-        <section class="row">
-            <div class="col-md-12">
-                @if (session('status'))
-                    <div class="alert alert-success alert-dismissible show fade">
-                        {{ session('status') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
 
-                <div class="card">
-                    <div class="card-body">
-                        <h4>Hi 👋, {{ auth()->user()->name }}</h4>
-                        <p>{{ __('You are logged in!') }}</p>
+                    <div class="h-100">
+                        <div class="row mb-3 pb-1">
+                            <div class="col-12">
+                                <div class="d-flex align-items-lg-center flex-lg-row flex-column">
+                                    <div class="flex-grow-1">
+                                        <h4 class="fs-16 mb-1">Welcome, {{ Auth::user()->name }}</h4>
+                                    </div>
+                                    <div class="mt-3 mt-lg-0">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <!-- card -->
+                                <div class="card card-animate">
+                                    <div class="card-body">
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Total
+                                                    Work Order</p>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex align-items-end justify-content-between mt-4">
+                                            <div>
+                                                <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                        data-target="12"></span></h4>
+                                            </div>
+                                            <div class="avatar-sm flex-shrink-0">
+                                                <span class="avatar-title bg-warning rounded fs-3">
+                                                    <i class="bx bx-user-circle"></i>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- end col -->
+                            <div class="col-xl-3 col-md-6">
+                                <!-- card -->
+                                <div class="card card-animate">
+                                    <div class="card-body">
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Total
+                                                    Equiment</p>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex align-items-end justify-content-between mt-4">
+                                            <div>
+                                                <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                        data-target="12"></span></h4>
+                                            </div>
+                                            <div class="avatar-sm flex-shrink-0">
+                                                <span class="avatar-title bg-success rounded fs-3">
+                                                    <i class="bx bx-dollar-circle"></i>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- end col -->
+
+                            <div class="col-xl-3 col-md-6">
+                                <!-- card -->
+                                <div class="card card-animate">
+                                    <div class="card-body">
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Total
+                                                    Employee</p>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex align-items-end justify-content-between mt-4">
+                                            <div>
+                                                <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                        data-target="10"></span></h4>
+                                            </div>
+                                            <div class="avatar-sm flex-shrink-0">
+                                                <span class="avatar-title bg-info rounded fs-3">
+                                                    <i class="bx bx-shopping-bag"></i>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- end col -->
+
+                            <div class="col-xl-3 col-md-6">
+                                <!-- card -->
+                                <div class="card card-animate">
+                                    <div class="card-body">
+                                        <div class="d-flex align-items-center">
+                                            <div class="flex-grow-1 overflow-hidden">
+                                                <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Total
+                                                    Vendor</p>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex align-items-end justify-content-between mt-4">
+                                            <div>
+                                                <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value"
+                                                        data-target="12"></span></h4>
+                                            </div>
+                                            <div class="avatar-sm flex-shrink-0">
+                                                <span class="avatar-title bg-danger rounded fs-3">
+                                                    <i class="bx bx-wallet"></i>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- end col -->
+
+
+                        </div>
+
+                        {{-- grafik knob --}}
+                        <div class="row">
+                            <div class="col-xl-4 col-md-4">
+                                <div class="card" style="height: 450px">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h4 class="card-title mb-0 flex-grow-1">
+                                            Percentage Status WO
+                                        </h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="table-responsive table-card">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-md-4">
+                                <div class="card" style="height: 450px">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h4 class="card-title mb-0 flex-grow-1">
+                                            Percentage Category WO
+                                        </h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="table-responsive table-card">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-md-4">
+                                <div class="card" style="height: 450px">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h4 class="card-title mb-0 flex-grow-1">
+                                            Percentage Type WO</h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="table-responsive table-card">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- map --}}
+                        <div class="row">
+                            <div class="col-xl-6 col-md-6">
+                                <div class="card" style="height: 450px">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h4 class="card-title mb-0 flex-grow-1">Location Employee</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="map-embed" id="map" style="height:100%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-md-6">
+                                <div class="card" style="height: 450px">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h4 class="card-title mb-0 flex-grow-1">Location Vendor</h4>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="map-embed" id="map" style="height:100%"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-4 col-md-4">
+                                <div class="card" style="height: 450px">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h4 class="card-title mb-0 flex-grow-1"><i
+                                                class="fa fa-cube text-success fs-3"></i>
+                                            Opname Sparepart
+                                        </h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="table-responsive table-card">
+                                            <table
+                                                class="table table-borderless table-hover table-nowrap align-middle mb-0 table-sm">
+                                                <thead class="table-light">
+                                                    <tr>
+                                                        <th>Sparepart Name</th>
+                                                        <th>Stock</th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Sep 20, 2021</td>
+                                                        <td>Sep 20, 2021</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-md-4">
+                                <div class="card" style="height: 450px">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h4 class="card-title mb-0 flex-grow-1"><i class="fa fa-sign-in text-success fs-3"
+                                                aria-hidden="true"></i> Stock In
+                                            Sparepart</h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="table-responsive table-card">
+                                            <table
+                                                class="table table-borderless table-hover table-nowrap align-middle mb-0 table-sm">
+                                                <thead class="table-light">
+                                                    <tr>
+                                                        <th>Sparepart Name</th>
+                                                        <th>Qty In</th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Sep 20, 2021</td>
+                                                        <td>Sep 20, 2021</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-md-4">
+                                <div class="card" style="height: 450px">
+                                    <div class="card-header align-items-center d-flex">
+                                        <h4 class="card-title mb-0 flex-grow-1"><i
+                                                class="fa fa-sign-out text-success fs-3" aria-hidden="true"></i> Stock Out
+                                            Sparepart</h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="table-responsive table-card">
+                                            <table
+                                                class="table table-borderless table-hover table-nowrap align-middle mb-0 table-sm">
+                                                <thead class="table-light">
+                                                    <tr>
+                                                        <th>Sparepart Name</th>
+                                                        <th>Qty Out</th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Sep 20, 2021</td>
+                                                        <td>Sep 20, 2021</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
     </div>
 @endsection
