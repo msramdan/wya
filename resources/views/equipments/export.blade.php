03e5d9d9438c1b9e315c92fbec2e22998b9e1637
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-04-02 13:45:27 +0700

    update

--- resources/views/equipments/export.blade.php
+++ resources/views/equipments/export.blade.php
@@ -1,7 +1,9 @@
 <table>
     <thead>
         <tr>
-            <th style="background-color:#D3D3D3 ">{{ __('Hospital') }}</th>
+            @if (Auth::user()->roles->first()->hospital_id)
+                <th style="background-color:#D3D3D3 ">{{ __('Hospital') }}</th>
+            @endif
             <th style="background-color:#D3D3D3 ">{{ __('Barcode') }}</th>
             <th style="background-color:#D3D3D3 ">{{ __('Nomenklatur') }}</th>
             <th style="background-color:#D3D3D3 ">{{ __('Equipment Category') }}</th>
@@ -10,15 +12,19 @@
             <th style="background-color:#D3D3D3 ">{{ __('Serial Number') }}</th>
             <th style="background-color:#D3D3D3 ">{{ __('Vendor') }}</th>
             <th style="background-color:#D3D3D3 ">{{ __('Condition') }}</th>
-            <th style="background-color:#D3D3D3 ">{{ __('Risk Level') }}</th>
             <th style="background-color:#D3D3D3 ">{{ __('Equipment Location') }}</th>
             <th style="background-color:#D3D3D3 ">{{ __('Financing Code') }}</th>
+            <th style="background-color:#D3D3D3 ">{{ __('Tanggal Pembelian') }}</th>
+            <th style="background-color:#D3D3D3 ">{{ __('Harga Pendapatan') }}</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($data as $dt)
             <tr>
-                <td>{{ $dt->hospital->name }}</td>
+
+                @if (Auth::user()->roles->first()->hospital_id)
+                    <td>{{ $dt->hospital->name }}</td>
+                @endif
                 <td>{{ $dt->barcode }}</td>
                 <td>{{ $dt->nomenklatur->name_nomenklatur }}</td>
                 <td>{{ $dt->equipment_category->category_name }}</td>
@@ -27,9 +33,10 @@
                 <td>{{ $dt->serial_number }}</td>
                 <td>{{ $dt->vendor->name_vendor }}</td>
                 <td>{{ $dt->condition }}</td>
-                <td>{{ $dt->risk_level }}</td>
                 <td>{{ $dt->equipment_location->location_name }}</td>
                 <td>{{ $dt->financing_code }}</td>
+                <td>{{ $dt->tgl_pembelian }}</td>
+                <td>{{ $dt->nilai_perolehan }}</td>
             </tr>
         @endforeach
     </tbody>
