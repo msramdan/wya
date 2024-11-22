e6d6300eef474e642b44e8b8bebf27cc48862b3a
Saepul Ramdan <saepulramdan244@gmail.com>
2024-11-22 07:37:05 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -27,14 +27,14 @@
   <header id="header" class="header d-flex align-items-center fixed-top">
     <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
-      <a href="index.html" class="logo d-flex align-items-center me-auto">
+      <a href="" class="logo d-flex align-items-center me-auto">
         <img src="{{asset('landing')}}/assets/img/logo.png" alt="">
         <h1 class="sitename">WithYouAlways</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="index.html#hero" class="active">Home</a></li>
+          <li><a href="" class="active">Home</a></li>
           <li><a href="">List Aduan</a></li>
           <li><a href="">Form Aduan</a></li>
           <li><a href="">Aduan Private</a></li>
